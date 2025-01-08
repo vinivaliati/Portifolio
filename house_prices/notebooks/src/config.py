cfbe4da7dd7b48ffb8817a7e67257ee4de530b4d
vinivaliati <vinivaliaticos@gmail.com>
2025-01-08 19:31:17 -0300

    threshold model trained

--- house_prices/notebooks/src/config.py
+++ house_prices/notebooks/src/config.py
@@ -7,6 +7,7 @@ PASTA_DADOS = PASTA_PROJETO / "dados"
 
 # coloque abaixo o caminho para os arquivos de dados de seu projeto
 dados_teste = PASTA_DADOS / "test.csv"
+dados_treino = PASTA_DADOS / "train.csv"
 
 # coloque abaixo o caminho para os arquivos de modelos de seu projeto
 PASTA_MODELOS = PASTA_PROJETO / "modelos"
